f58581347446393cc8035c1a569763e08279e464
Bravolakmedia <olakunlebravo@gmail.com>
2025-11-19 03:33:40 +0100

    bug: Fixed import error in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useWallet } from "../context/walletContext";
+import { useWallet } from "../context/WalletContext";
 import FhirForm, { type FhirFormData } from "../components/FhirForm";
 import { sha256Hex } from "../utils/hash";
 import ProvidersPanel from "../components/ProvidersPanel";
